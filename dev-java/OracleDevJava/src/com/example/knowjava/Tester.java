2d9ea4d2075b8d67dedcfe135c95f566cf090fab
akashydv311 <akashydv311@gmail.com>
2024-10-12 19:02:24 +0530

    add updated folder

--- dev-java/OracleDevJava/src/com/example/knowjava/Tester.java
+++ dev-java/OracleDevJava/src/com/example/knowjava/Tester.java
@@ -1,13 +1,12 @@
-package com.oracle.firstapp;
+package com.example.knowjava;
 
 public class Tester {
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, World!");
-		
 		String currJavaRuntime = System.getProperty("java.runtime.version");
 		
 		System.out.println("Current Java Runtime Version is: "+currJavaRuntime);
+		
 		
 	}
 }
---
+++ dev-java/OracleDevJava/src/com/example/knowjava/package-info.java
@@ -0,0 +1,22 @@
+package com.example.knowjava;
+
+/*
+
+Resource For This [ https://dev.java/learn/ ]
+
+This package contains
+	
+Running Your First Java Application
+	Getting Started with Java
+	Launching Single-File Source-Code Programs
+	JShell - The Java Shell Tool
+	Building a Java Application in the Eclipse IDE
+	
+
+Staying Aware of New Features
+	Java Platform Evolution
+	Using the Preview Features Available in the JDK
+	
+
+
+*/
