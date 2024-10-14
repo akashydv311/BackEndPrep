c82777bb6e5a349e8137692e54274807e7f9f251
akashydv311 <akashydv311@gmail.com>
2024-10-15 00:13:58 +0530

    add sql query

---
+++ dev-sql/mysql/Aggregation_Grouping.sql
@@ -0,0 +1,164 @@
+
+SHOW DATABASES;
+USE db2;
+SHOW TABLES;
+
+SELECT * FROM Learners;
+
+-- Count the number of student who joined the course via LinkedIn, YouTube, Community
+
+SELECT SourceOfJoinning, COUNT(*) AS Student_Join
+FROM Learners
+GROUP BY SourceOfJoinning;
+
+-- NOTE: After SELECT command 
+	-- Non aggrigated column can be written but they must be written after group by as well
+    -- Aggrigated column can be written after SELECT 
+
+ 
+SELECT SourceOfJoinning, Location
+FROM Learners
+GROUP BY SourceOfJoinning, Location;
+
+-- Corresponding to each course, How maney students have enrolled
+SELECT * FROM Learners;
+
+SELECT SelectedCourse, COUNT(*) AS student
+FROM Learners
+GROUP BY SelectedCourse;
+
+-- SELECT CourseName, COUNT(*) AS Enrolled_Student
+-- FROM Courses
+-- GROUP BY CourseName;
+
+-- Corresponding to individual source of joinning, give the maximum years of experiance any person have
+SELECT * FROM learners;
+
+SELECT SourceOfJoinning, MAX(YearsOfExp)
+FROM learners
+GROUP BY SourceOfJoinning;
+
+SELECT SourceOfJoinning, MIN(YearsOfExp)
+FROM learners
+GROUP BY SourceOfJoinning;
+
+SELECT SourceOfJoinning, AVG(YearsOfExp)
+FROM learners
+GROUP BY SourceOfJoinning;
+
+SELECT SourceOfJoinning, SUM(YearsOfExp)
+FROM learners
+GROUP BY SourceOfJoinning;
+
+SELECT * FROM Learners;
+SELECT * FROM Employee;
+SELECT * FROM Courses;
+
+-- Display the records of those learners who have joined  the course via more then 1 source of joinning 
+-- After group by we have to use Having clause not whare for filter
+
+SELECT  SourceOfJoinning, COUNT(*) AS Std_learners
+FROM Learners
+GROUP BY SourceOfJoinning
+HAVING Std_learners > 1;
+
+-- Display the count of those students who have joined via linkedIn
+SELECT SourceOfJoinning , COUNT(*) AS LinkedIn
+FROM Learners
+GROUP BY SourceOfJoinning
+HAVING SourceOfJoinning = 'YouTube';
+
+-- Logical Operators
+
+-- Display the course which doesn't include 'Excel'
+SELECT * FROM Courses;
+
+SELECT * FROM Courses
+WHERE CourseName NOT LIKE '%Excal%';
+
+-- Display the records of those students who have less then 4 years of experiance and source of joinning is youtube and location is channai
+SELECT * FROM Learners;
+
+SELECT * FROM Learners
+WHERE YearsOfExp < 4 AND SourceOfJoinning = 'YouTube' AND Location = 'Chennai';
+
+-- Display the records of those students who have years of experiance Bitween 1 to 3 years
+SELECT * FROM Learners
+WHERE YearsOfExp BETWEEN 1 AND 3;
+
+-- ALTER COMMAND IN SQL
+
+DESC Employee; 
+
+ALTER TABLE Employee ADD COLUMN 
+JobPosition VARCHAR(50);
+
+ALTER TABLE Employee ADD COLUMN 
+Temp VARCHAR(50);
+
+ALTER TABLE Employee MODIFY 
+FirstName VARCHAR(22);
+
+ALTER TABLE Employee DROP
+Temp;
+
+ALTER TABLE COURSES RENAME Course;
+
+-- TRUNCATE IN SQL
+-- DELETE (DML) VS TRUNCATE (DDL)
+
+
+
+-- DATATYPES IN SQL
+-- INT, VARCHAR, TIMESTAMP, FOAT
+
+DESC Course;
+
+CREATE TABLE Temp1(
+StudentName VARCHAR(20),
+StudentAge DECIMAL(3,1)
+);
+
+DESC Temp1;
+
+INSERT INTO Temp1 VALUES ('Akash', 23.5);
+INSERT INTO Temp1 VALUES ('Himanshu', 26.8);
+
+SELECT * FROM Temp1;
+
+TRUNCATE TABLE Temp1;
+
+DROP TABLE Temp1;
+
+
+
+CREATE TABLE Course_Update(
+CourseID INT AUTO_INCREMENT,
+CourseName VARCHAR(50) NOT NULL,
+CourseDuration DECIMAL(3,1) NOT NULL,
+CourseFee INT NOT NULL,
+Changed_AT TIMESTAMP DEFAULT NOW() ON UPDATE NOW(),
+PRIMARY KEY (CourseID)
+);
+
+DROP TABLE Course_Update;
+
+INSERT INTO Course_Update(CourseName, CourseDuration, CourseFee) 
+VALUES("DSA Interview Prepration", 3.4, 4999),
+("Excal Mastrey", 2.1, 3000),
+("SQL Complete Mastrey", 5.0, 6500);
+
+INSERT INTO Course_Update(CourseName, CourseDuration, CourseFee) VALUES("Object Oriented Programming", 1.4, 4999);
+
+SELECT * FROM Course_Update;
+
+-- UPDATE COMMAND
+
+UPDATE Course_Update SET CourseFee = 3600
+WHERE CourseID=4;
+
+
+
+
+
+  
---
+++ dev-sql/mysql/ForeignKey.sql
@@ -0,0 +1,108 @@
+SHOW DATABASES;
+USE db2;
+SHOW TABLES;
+
+CREATE TABLE Courses(
+CourseID INT AUTO_INCREMENT,
+CourseName VARCHAR(50) NOT NULL,
+CourseDuration INT NOT NULL,
+CourseFee INT NOT NULL,
+PRIMARY KEY (CourseID)
+);
+
+INSERT INTO Courses(CourseName, CourseDuration, CourseFee) 
+VALUES("DSA Interview Prepration", 3, 4999),
+("Excal Mastrey", 2, 3000),
+("SQL Complete Mastrey", 5, 6500);
+
+DELETE FROM Courses
+WHERE CourseID != 0;
+DROP TABLE courses;
+
+SELECT * FROM Courses;
+
+DESC Courses;
+
+CREATE TABLE Learners(
+LearnerID INT AUTO_INCREMENT,
+LearnerFirstName VARCHAR(50) NOT NULL,
+LearnerLastName VARCHAR(50) NOT NULL,
+LearnerEmailID VARCHAR(50) NOT NULL,
+LearnerPhone VARCHAR(50) NOT NULL,
+LearnerEnrollmentDate TIMESTAMP NOT NULL,
+SelectedCourse INT NOT NULL,
+YearsOfExp INT,
+SourceOfJoinning VARCHAR(50) NOT NULL,
+LearnerCompany VARCHAR(50) NOT NULL,
+BatchStartDate TIMESTAMP NOT NULL,
+Location VARCHAR(50) NOT NULL,
+
+PRIMARY KEY (LearnerID),
+UNIQUE KEY (LearnerEmailID),
+FOREIGN KEY (SelectedCourse) REFERENCES Courses (CourseID)
+);
+
+-- Batch Start Date '1' = '2024-02-29'
+-- Batch Start Date '2' = '2024-01-16'
+-- Batch Start Date '3' = '2024-03-25'
+
+INSERT INTO Learners(LearnerFirstName, LearnerLastName, LearnerEmailID, LearnerPhone, LearnerEnrollmentDate, SelectedCourse, YearsOfExp, SourceOfJoinning, LearnerCompany, BatchStartDate, Location)
+VALUES
+("Akash", "Yadav", "ay36808@gmail.com", "6261626100", '2024-02-27', 1, 2, "Youtube", "Infosys", '2024-02-29', "Bhopal"),
+("Akhil", "George", "akhil.george.8743@gmail.com", "1234567890", '2024-01-14', 2, 6, "Linkdin", "TCS", '2024-01-16', "Benguluru"),
+("Rishikesh", "Joshi", "carjkop@gmail.com", "9087654321", '2024-03-17', 3, 15, "Community", "Amazon", '2024-03-25', "Pune"),
+("Madhuram", "Ravichadran", "ravichadran.madhuram@gmail.com", "2345676432", '2024-02-22', 1, 4, "Linkedin", "Wipro", '2024-02-29', "Indore"),
+("Jeevan", "Hegde", "jeevanhegade@gmail.com", "9923452345", '2024-01-11', 2, 18, "Youtube", "Congni", '2024-01-16', "Chennai"),
+("Sidhish", "Kumar", "sidhishkumar@gmail.com", "9876234523", '2024-03-12', 3, 3, "Linkdin", "Google", '2024-03-25', "Jaipur"),
+("Naga Sai", "Sreedhar", "saisreedhar2001@gmail.com", "3564897654", '2024-02-10', 1, 14, "Youtube", "Virtusha", '2024-02-29', "Noida");
+
+SELECT * FROM Learners;
+
+-- Data analysis [ Employee, Course, Learners]
+-- 1. Give me the record of the employee getting highest salary
+DESC Employee;
+SELECT * FROM Employee;
+
+SELECT * FROM Employee
+ORDER BY Salary DESC
+LIMIT 1;  
+
+-- 2. Give me the record of the employee getting highest salary and age is bigger then 23
+ SELECT * FROM Employee
+ WHERE Age>23
+ ORDER BY Salary DESC;
+ 
+ -- 3. Display the number of enrollments in the website of the learners
+ SELECT COUNT(*) AS Number_Of_Enrollments FROM Learners;
+ 
+ -- 4. Display the number of enrollments for the course id 3
+ SELECT COUNT(*) AS SQL_Course FROM Learners
+ WHERE SelectedCourse=3;
+
+
+-- 5. Count the number of learners enrolled in the month of jan 21
+SELECT COUNT(*) AS STD_JAN FROM Learners 
+WHERE LearnerEnrollmentDate LIKE '%-01-14%';
+
+
+SELECT * FROM Learners;
+-- 6. Update the jeevan data with year of experience as 1 and compney as Amazon
+UPDATE Learners
+SET YearsOfExp=2 , LearnerCompany='Amazon'
+WHERE LearnerID=5;
+
+
+-- 7. Count the number of companies where learners doing their job
+-- Count is count the non null entries in the table
+ 
+SELECT Count(LearnerCompany) FROM Learners;  
+
+
+
+
+
+
+
+
+
+
