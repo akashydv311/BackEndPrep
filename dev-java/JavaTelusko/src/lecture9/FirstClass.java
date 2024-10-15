8b4f0ce67ab841911a49449320595adadf96d942
akashydv311 <akashydv311@gmail.com>
2024-10-15 23:59:34 +0530

    add code

--- dev-java/JavaTelusko/src/lecture9/FirstClass.java
+++ dev-java/JavaTelusko/src/lecture9/FirstClass.java
@@ -1,5 +1,26 @@
 package lecture9;
 
-public class FirstClass {
+// local variable and instance variable
+class Alien{
+	// instance variable
+	int age;
+	double salary;
+	
+	// local variable
+	void show() {
+		String name;
+//		System.out.println(name); // local varibale must have init
+		name = "TestName";
+		System.out.println(name+" "+age+" "+salary);
+	}
+}
 
+public class FirstClass {
+	public static void main(String[] args) {
+		
+		// Crating object of Alien
+		Alien a = new Alien();
+		a.show();
+		
+	}
 }
