d6a9d85cf126b6c9d95c5651b5a14f1fea8153cc
akashydv311 <akashydv311@gmail.com>
2024-12-03 23:38:58 +0530

    add dynamic input jdbc

---
+++ dev-java/JDBC/JDBCAPP/src/dynamicinput/DeleteApp.java
@@ -0,0 +1,90 @@
+package dynamicinput;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class DeleteApp {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Scanner sc = new Scanner(System.in);
+		Connection connection = null;
+		Statement statement = null;
+		
+		// load and register the driver
+		// automatic
+		
+		// 2.getConnection
+		String url = "jdbc:mysql://localhost:3306/jdbcdb";
+		String user = null;
+		String pass = null;
+		try {
+			
+			System.out.println("Enter DB user name: ");		
+			user = sc.next();
+			
+			System.out.println("Enter DB password: ");
+			pass = sc.next();
+			
+		}catch (Exception e) {
+			System.out.println("Please insert data proprely");
+		}
+		
+		
+		try {
+			
+			connection = DriverManager.getConnection(url, user, pass);
+			
+			if(connection!=null) {
+				// 3. Query Statement
+				statement = connection.createStatement();
+				
+				if(statement!=null) {
+					// 4. Execute 
+					System.out.println("Enter id for dalete record: ");
+					int id = sc.nextInt();
+					
+					String updateQuery = String.format("DELETE from player where pid=%d", id);
+					int noOfRow = statement.executeUpdate(updateQuery);
+					
+					System.out.println("Number of now affected: "+noOfRow);
+					
+				}
+			}
+			
+		}catch (SQLException ex) {
+			ex.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			
+			int count = 0;
+			
+			if(statement!=null) {
+				statement.close();
+				++count;
+			}
+			if(connection!=null) {
+				connection.close();
+				++count;
+			}
+			if(sc!=null) {
+				sc.close();
+				++count;
+			}
+			if(count==3) {
+				System.out.println("\n*******All resources close!*******");
+			}else {
+				System.out.println("\n*******All resourses not closed!******");
+			}
+	
+		}
+		
+		
+		
+	}
+	
+}
