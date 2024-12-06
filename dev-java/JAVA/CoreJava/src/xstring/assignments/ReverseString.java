526a4c79a4abdcf4d2a9ddf8402e93923df8b4f3
akashydv311 <akashydv311@gmail.com>
2024-12-06 23:04:35 +0530

    add string assignment:

---
+++ dev-java/JAVA/CoreJava/src/xstring/assignments/ReverseString.java
@@ -0,0 +1,23 @@
+package xstring.assignments;
+
+// WAP to reverse a String.
+//Input: “iNeuron”
+//Output: “norueNi”
+
+public class ReverseString {
+	public static void main(String[] args) {
+		
+		String s = "iNeuron";
+		String arr[]= null;
+		arr = s.split("");
+		
+		for(int i=0; i<arr.length; i++) {
+			
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+		}
+		
+	}
+}
