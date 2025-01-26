7c645371ae425c7c7722b3c9e4cbff4ef1e35e8f
akashydv311 <akashydv311@gmail.com>
2025-01-26 21:32:27 +0530

    added interfaces concept

---
+++ dev-java/JAVA/CoreJava/src/oop/basic/interfaceconcept/InterfaceExample.java
@@ -0,0 +1,32 @@
+package oop.basic.interfaceconcept;
+
+// Interfaces Are Meant for Abstraction and Flexibility
+
+interface Human{
+	
+	// by default take the public and abstract for no body methods
+	void test01(); // default method
+	
+	abstract void test02(); // abstract method
+	
+//	private void test03(); // private AM
+	
+//	protected void test04(); // protected AM
+	
+	public static void test05() {	} // static method
+	
+//	public final void test06() {   }; // final method
+	
+//	public void test06() {}; // regular method
+	
+}
+
+
+
+public class InterfaceExample {
+	public static void main(String[] args) {
+	
+		
+		
+	}
+}
